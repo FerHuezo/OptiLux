3b385a1c4fc0e2afeb71e9ae376c16811431a3f3
user name <user email>
2025-03-22 17:54:37 -0600

    first commit

---
+++ backend/database.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+import { config } from "./src/config.js";
+
+
+//conectar la base de datos
+//esta dirección se ubica en la carpeta config
+mongoose.connect(config.db.URI);
+
+//conectada
+const connection = mongoose.connection;
+connection.once("open", ()=> {
+    console.log("DB is connected");
+});
+//desconectada
+connection.on("disconnected", ()=>{
+    console.log("DB is disconnected");
+});
+//error
+connection.on("error", (error)=>{
+    console.log("error found: " + error);
+});
