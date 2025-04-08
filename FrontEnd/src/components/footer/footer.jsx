52853486fd611d60f7002055aa9fb1ca7dc7de25
Nataren <20220594@ricaldone.edu.sv>
2025-04-08 00:05:13 -0600

    component footer terminado

--- FrontEnd/src/components/footer/footer.jsx
+++ FrontEnd/src/components/footer/footer.jsx
@@ -1,26 +1,37 @@
 import React from "react";
 import '../footer/footer.css'
+import Lentes from "./img/lentes-de-contacto.png";
 
 const Footer = () => {
 
     return(
         <>
+        
         <footer>
-        <div class="container">
-            <div class="footer-info">
-                <h3 id="contactanos">Contacto</h3>
-                <p>OptiLux</p>
-                <p>Ricaldone, El salvador, San Salvador</p>
-                <p>Email: Optilux_NHC@gmail.com</p>
-                <p>Teléfono: +503 7233 2013</p>
+        <div className="Container-P">
+                <a href="#" target="_blank" rel="noopener noreferrer"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/2023_Facebook_icon.svg/2048px-2023_Facebook_icon.svg.png" alt="Facebook"/></a>
+                <a href="#" target="_blank" rel="noopener noreferrer"><img src="https://images.freeimages.com/image/large-previews/f35/x-twitter-logo-on-black-circle-5694247.png" alt="X"/></a>
+                <a href="#" target="_blank" rel="noopener noreferrer"><img src="https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-instagram-new-circle-512.png" alt="Instagram"/></a>
+                <a href="#" target="_blank" rel="noopener noreferrer"><img src="https://cdn2.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-snapchat-circle-512.png" alt="Snapchat"/></a>
+                <a href="#" target="_blank" rel="noopener noreferrer"><img src="https://cdn-icons-png.flaticon.com/512/145/145808.png" alt="Pinterest"/></a>
+        </div>
+        
+        <div className="container">
+            <div className="footer-info">
+                <img src={Lentes} alt="Icono Lentes"/>
+                <div className="contenedor-separadorA"> 
+                    <div className="lineaSeparadora"></div>
+                </div>
+                <div className="info-secundaria">
+                    <p>Muchas gracias por visitar nuestra tienda y tener la comodidad de preferinos, nos comprometemos a dar el mejor serivcio con nosotros <strong>¡OptiLux!</strong>.</p>
+                    <p>¡Regresa a nuestra pagina de <a href="">Inicio</a> para poder ver los diferentes productos y descuentos!</p>
+                    <br/>
+                    <p>Ricaldone - El salvador, San Salvador #3125 - +503 7233 2013 -  <a href="">Optilux_NHC@gmail.com</a></p>
+                    <p>&copy; 2025 Optilux</p>
+                </div>
+                
             </div>
-    
-            <div class="footer-Optilux">
-                <h3>Síguenos</h3>
-                <a href="#" target="_blank" rel="noopener noreferrer"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Logo_de_Facebook.png/220px-Logo_de_Facebook.png" alt="Facebook"/></a>
-                <a href="#" target="_blank" rel="noopener noreferrer"><img src="https://t.ctcdn.com.br/b2gehl3ZRAKUVpehWpv4ZR-yBvE=/i489929.jpeg" alt="Twitter"/></a>
-                <a href="#" target="_blank" rel="noopener noreferrer"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png" alt="Instagram"/></a>
-            </div>
+               
         </div>
         </footer>
         </>
