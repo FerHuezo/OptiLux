fca389d70e6d545fa222d093ca7de0ed6e6ce989
user name <user email>
2025-04-27 19:00:44 -0600

    navegar por productos terminado

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import LogIn from './pages/LogIn'
 import Contactanos from './pages/Contactanos/contactanos'
 import Politica from './Pages/Politica/politica'
+import Pimportados from './Pages/Productos/pImportados';
 
 const App = () => {
   return (
@@ -12,7 +13,7 @@ const App = () => {
       <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/Ofertas" element={<h1>Ofertas</h1>} />
-        <Route path="/Productos" element={<h1>Productos</h1>} />
+        <Route path="/productos" element={<Pimportados/>} />
         <Route path="/Login" element={<LogIn />}/>
         <Route path="/Contactanos" element={<Contactanos />}/>
         <Route path="/Politica" element={<Politica/>}/>
