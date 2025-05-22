99ca6afb8c42243b8d0c898ee98d54b2d29d041f
Natraidor <nata39520@gmail.com>
2025-05-22 17:35:12 -0600

    creando login

--- FrontEnd/web-private/src/App.jsx
+++ FrontEnd/web-private/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import PedidosActivos from './pages/Pedidos/Pedidos';
 import ProductoFiltro from './pages/prodcutoFiltros/productoFiltro';
 import ProductoAros from './pages/productoAros/productoAros';
@@ -7,12 +7,31 @@ import ProductosImportados from './pages/productosImportados/productosImportados
 import ProductoAumento from './pages/productoAumento/productoAumento'
 import ProductoTerminales from './pages/productoTerminales/productoTerminales'
 import DetallePedido from './pages/DetallePedido/DetallePedido';
+import Sidebar from './components/SideBar/SideBar';
+
+import { PrivateRoute } from './components/privateRoute';
+import { useAuth } from './context/AuthContext';
+import { use } from 'react';
 
 const App = () => {
+
+  const { authCokie } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (authCokie) {
+      navigate("/Productos");
+    }
+  }, [authCokie]);
+
   return (
     <>
     <Router>
       <Routes>
+
+        {!authCokie ? <Route path="/login" element={<Login />} /> :null}
+
+        <Route element = {<PrivateRoute/>}>
         <Route path="/Productos" element={<ProductosImportados/>}/>
         <Route path="/Productos/Filtros" element={<ProductoFiltro/>}/>
         <Route path="/Productos/Aros" element={<ProductoAros/>}/>
@@ -20,6 +39,7 @@ const App = () => {
         <Route path="/Productos/Terminales" element={<ProductoTerminales/>}/>
         <Route path="/Pedidos" element={<PedidosActivos/>}/>
         <Route path="/Pedidos/1" element={<DetallePedido/>}/>
+        </Route>
       </Routes>
     </Router>
     </>
---
+++ FrontEnd/web-private/src/context/AuthContext.jsx
@@ -0,0 +1,67 @@
+import React, { use } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
+
+
+const SERVER_URL = "http://localhost:4000/api";
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [authCokie, setAuthCokie] = useState(null);
+
+      const Login = async (email, password) => {
+    try {
+      const response = await fetch(`${SERVER_URL}/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || "Error en la autenticación");
+      }
+
+      const data = await response.json();
+      localStorage.setItem("authToken", data.token);
+      localStorage.setItem("user", JSON.stringify({ email }));
+      setAuthCokie(data.token);
+      setUser({ email });
+
+      return { success: true, message: data.message };
+
+    } catch (error) {
+      return { success: false, message: error.message };
+    }
+  };
+
+    const logout = async () => {
+
+    localStorage.removeItem("authToken");
+    localStorage.removeItem("user");
+    setAuthCokie(null);
+    setUser(null);
+  };
+
+  // En useEffect
+  useEffect(() => {
+    const token = localStorage.getItem("authToken");
+    const savedUser = localStorage.getItem("user");
+    if (token) {
+      setAuthCokie(token);
+      if (savedUser) {
+        setUser(JSON.parse(savedUser));
+      }
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ user, Login, logout, authCokie, setAuthCokie }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => { useContext(AuthContext) };
