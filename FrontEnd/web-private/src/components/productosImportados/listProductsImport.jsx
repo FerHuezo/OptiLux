a3b6d9c99ff4ecf732c023e75de24920f39b7548
natraidor <20220594@ricaldone.edu.sv>
2025-05-26 10:00:01 -0600

    pruebas

---
+++ FrontEnd/web-private/src/components/productosImportados/listProductsImport.jsx
@@ -0,0 +1,26 @@
+import productCard from "./productImportCard";
+import React from "react";
+
+const listProduct=({})=>{
+    return(
+        <div className="">
+        <h1 className="text-2xl font-bold underline text-center">
+          Listado de marcas
+        </h1>
+        <div className="flex flex-wrap gap-4 justify-center mt-5">
+          {loading && <div className="text-center text-gray-500">Loading...</div>}
+  
+          {brands?.map((brand) => (
+            <BrandCard
+              key={brand._id}
+              brand={brand}
+              deleteBrand={deleteBrand}
+              updateBrands={updateBrands}
+            />
+          ))}
+        </div>
+      </div>
+    );
+}
+
+export default listProduct;
